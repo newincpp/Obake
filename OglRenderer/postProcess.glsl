33785020b321b31525d05d90517403bb32d576c7
newin <newin@legion.ch>
2016-08-07 14:47:52 +0200

    deleting depth for tests on firepro

--- OglRenderer/postProcess.glsl
+++ OglRenderer/postProcess.glsl
@@ -7,7 +7,7 @@ uniform float uTime;
 uniform sampler2D gPosition;
 uniform sampler2D gNormal;
 uniform sampler2D gAlbedoSpec;
-uniform sampler2D gDepth;
+//uniform sampler2D gDepth;
 
 in vec2 TexCoords;
 
@@ -18,6 +18,6 @@ void main() {
     vec3 cp = texture(gPosition, distCoord).rgb;
     vec3 cn = texture(gNormal, distCoord - vec2(1.0f,0.0f)).rgb;
     vec3 ca = texture(gAlbedoSpec, distCoord - vec2(0.0f,1.0f)).rgb;
-    vec3 cd = texture(gDepth, distCoord - vec2(1.0f,1.0f)).rgb;
-    outColour = cn + cp + ca + cd;
+    //vec3 cd = texture(gDepth, distCoord - vec2(1.0f,1.0f)).rgb;
+    outColour = cn + cp + ca;// + cd;
 }
